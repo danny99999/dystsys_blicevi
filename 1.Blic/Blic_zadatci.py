21e799f317cc325dac4b38c4098a365e136468fc
danny99999 <danijeldupor.dd@gmail.com>
2022-12-28 20:27:35 +0100

    1.Blic_rjesenje

---
+++ 1.Blic/Blic_zadatci.py
@@ -0,0 +1,26 @@
+# 1. Zadatak
+def funkcija(x):
+    assert isinstance(x, list) and all (isinstance (i,str) for i in x)
+    return {k:v[::-1] for k,v in enumerate(x)}
+
+#2. Zadatak
+
+def funkcija2(x, y):
+    assert isinstance(x, dict) and isinstance(y, dict)
+    assert all([isinstance(i, list) for _, i in x.items()]) and all([isinstance(i, list) for _, i in y.items()])
+    assert(x.get("valute") and x.get("cijena")) and (y.get("valute") and y.get("cijena"))
+    return[(a,b) for a,d,b,c in zip(x["cijena"], y["cijena"], x["valute"], y["valute"]) if (a==d) and (b==c)]  
+
+# 3. Zadatak
+
+def funkcija3(x):
+    assert isinstance(x, list) and all([isinstance(y, dict) for y in x])
+    c= {k for d in x for a,k in d.items() if a== "kategorija"}
+    return{k:sum([v["ocjena"] for v in x if v["kategorija"] == k]) for d in x for _, k in d.items() if k in c}
+
+print (funkcija(['Stol', 'Stolica', 'Krevet', 'Fotelja']))    
+print(funkcija2({"valute":["GBR", "USD", "CZK", "Error"], "cijena":[8.5, 7.7, 0.3, 10.3]}, {"valute":["EUR", "USD", "CZK", "Error"], 
+"cijena":[7.5, 7.7, 0.3, 5.5]}))
+print(funkcija3([{"naziv":"Burek", "kategorija":"pite", "ocjena":1}, {"naziv":"Ramsteak", "kategorija":"steak", "ocjena":9}, 
+{"naziv": "Ribeye", "kategorija":"steak", "ocjena":4}, {"naziv":"Sirnica", "kategorija":"pite", "ocjena":5}]))
+
